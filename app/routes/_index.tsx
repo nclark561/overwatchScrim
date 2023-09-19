7256c81f11b1ff2a3acebefe263d9a3629fed686
Noah Clark <noahammon00@gmail.com>
2023-09-19 15:25:28 -0600

    added prisma to the project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="text-purple-700">Welcome to Remix</h1>
+      <h1 className="">Scrims Overwatch</h1>
     </div>
   );
 }
