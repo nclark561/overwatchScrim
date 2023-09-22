f97e37e9f2761e3ed79cc432fd531961097c128b
Noah Clark <noahammon00@gmail.com>
2023-09-21 19:01:56 -0600

    made username a unique property and populated seed function with users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,54 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
-import * as bcrypt from "bcryptjs"
+const prisma = new PrismaClient();
+import * as bcrypt from "bcryptjs";
+
+const salt = bcrypt.genSaltSync(10);
 
 const main = async () => {
-    
-}
+  const travis = await prisma.user.upsert({
+    where: { username: "travis" },
+    update: {},
+    create: {
+      username: "travis",
+      password: {
+        create: {
+          hash: bcrypt.hashSync("password182", salt),
+        },
+      },
+    },
+  });
+  const pig = await prisma.user.upsert({
+    where: { username: "pig" },
+    update: {},
+    create: {
+      username: "pig",
+      password: {
+        create: {
+          hash: bcrypt.hashSync("password1.", salt),
+        },
+      },
+    },
+  });
+  const phantom = await prisma.user.upsert({
+    where: { username: "phantom" },
+    update: {},
+    create: {
+      username: "phantom",
+      password: {
+        create: {
+          hash: bcrypt.hashSync("password44", salt),
+        },
+      },
+    },
+  });
+};
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
