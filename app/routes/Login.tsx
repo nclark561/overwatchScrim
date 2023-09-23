ba3a291ffb768c842fc0f51f4afc56893638a58e
Noah Clark <noahammon00@gmail.com>
2023-09-23 14:28:08 -0600

    completely reworked login page to use the remix Form component instead of using the native form with useState to control the values

--- app/routes/Login.tsx
+++ app/routes/Login.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Form } from "@remix-run/react";
+import { useActionData } from "@remix-run/react";
+import { ActionFunction } from "@remix-run/node";
 import { login } from "~/utils/sessions.server";
 
 type UserForm = {
@@ -7,41 +10,70 @@ type UserForm = {
   repass: string;
 };
 
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    username: string | undefined;
+    password: string | undefined;
+  };
+  fields?: {
+    loginType: string;
+    username: string;
+    password: string;
+    repass: string;
+  };
+};
+
+export const action: ActionFunction = async ({
+  request,
+})/*: Promise<Response | ActionData>*/ => {
+  let form = await request.formData()
+  let loginType = form.get("loginType")
+  let username = form.get("username")
+  let password = form.get("password")
+  let repass = form.get("repass")
+
+  let fields = {loginType, username, password, repass}
+  switch (loginType) {
+    case 'login':
+      console.log(form.get('username'), form.get('password'))
+      return fields
+    case 'register':
+      console.log(form.get('username'), form.get('password'), form.get("repass"))
+      return fields
+    default:
+      return {formError: 'login type invalid'}
+  }
+};
+
 export default function Login() {
   const [register, setRegister] = useState(false);
-  const [form, setForm] = useState<Partial<UserForm>>({});
   const handleClick = (evt: React.SyntheticEvent) => {
     evt.preventDefault();
     setRegister((prev) => !prev);
   };
-  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setForm((prev) => {
-      return {
-        ...prev,
-        [evt.target.name]: evt.target.value,
-      };
-    });
-  };
   return (
     <div>
-      <form>
+      <Form method="POST" preventScrollReset>
         <h1>{register ? "Register" : "Login"}</h1>
         <label htmlFor="username-in">username:</label>
-        <input id="username-in" name="username" onChange={handleChange} />
+        <input id="username-in" name="username" />
         <label htmlFor="password-in">password:</label>
-        <input id="password-in" name="password" onChange={handleChange} />
+        <input id="password-in" name="password" />
         {register && (
           <>
             <label htmlFor="repass-in">confirm password:</label>
-            <input id="repass-in" name="repass" onChange={handleChange} />
+            <input id="repass-in" name="repass" />
           </>
         )}
+        <input type="hidden" name="loginType" value={register ? 'register' : 'login'}/>
         <button onClick={handleClick}>
           {register
             ? "already have an account? click here"
             : "not registered yet? click here"}
         </button>
-      </form>
+        <input type="submit" />
+      </Form>
     </div>
   );
 }
