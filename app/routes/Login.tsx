9761d68b7581905177522d5ad2d447f204e45a54
Noah Clark <noahammon00@gmail.com>
2023-09-24 14:09:44 -0600

    fixed type error in the login component action function by adding a type check gate keeper conditional statement and cleaned up some extraneous code

--- app/routes/Login.tsx
+++ app/routes/Login.tsx
@@ -1,14 +1,7 @@
 import { useState } from "react";
-import { Form } from "@remix-run/react";
-import { useActionData } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import { ActionFunction } from "@remix-run/node";
 import { login } from "~/utils/sessions.server";
-
-type UserForm = {
-  username: string;
-  password: string;
-  repass: string;
-};
 
 type ActionData = {
   formError?: string;
@@ -26,28 +19,39 @@ type ActionData = {
 
 export const action: ActionFunction = async ({
   request,
-})/*: Promise<Response | ActionData>*/ => {
-  let form = await request.formData()
-  let loginType = form.get("loginType")
-  let username = form.get("username")
-  let password = form.get("password")
-  let repass = form.get("repass")
+}) /*: Promise<Response | ActionData>*/ => {
+  let form = await request.formData();
+  let loginType = form.get("loginType");
+  let username = form.get("username");
+  let password = form.get("password");
+  let repass = form.get("repass");
 
-  let fields = {loginType, username, password, repass}
+  if (
+    typeof loginType !== "string" ||
+    typeof username !== "string" ||
+    typeof password !== "string" ||
+    typeof repass !== "string"
+  ) {
+    return { formError: `Form not submitted correctly.` };
+  }
+
+  let fields = { loginType, username, password, repass };
   switch (loginType) {
-    case 'login':
-      console.log(form.get('username'), form.get('password'))
-      return fields
-    case 'register':
-      console.log(form.get('username'), form.get('password'), form.get("repass"))
-      return fields
+    case "login":
+      console.log(form.get("username"), form.get("password"));
+      const user = await login({ username, password });
+      return fields;
+    case "register":
+      console.log(username, password, repass);
+      return fields;
     default:
-      return {formError: 'login type invalid'}
+      return { formError: "login type invalid" };
   }
 };
 
 export default function Login() {
   const [register, setRegister] = useState(false);
+  const actionData = useActionData<ActionData | undefined>();
   const handleClick = (evt: React.SyntheticEvent) => {
     evt.preventDefault();
     setRegister((prev) => !prev);
@@ -57,16 +61,35 @@ export default function Login() {
       <Form method="POST" preventScrollReset>
         <h1>{register ? "Register" : "Login"}</h1>
         <label htmlFor="username-in">username:</label>
-        <input id="username-in" name="username" />
+        <input
+          type="text"
+          id="username-in"
+          name="username"
+          defaultValue={actionData?.fields?.username}
+        />
         <label htmlFor="password-in">password:</label>
-        <input id="password-in" name="password" />
+        <input
+          type="text"
+          id="password-in"
+          name="password"
+          defaultValue={actionData?.fields?.password}
+        />
         {register && (
           <>
             <label htmlFor="repass-in">confirm password:</label>
-            <input id="repass-in" name="repass" />
+            <input
+              type="text"
+              id="repass-in"
+              name="repass"
+              defaultValue={actionData?.fields?.repass}
+            />
           </>
         )}
-        <input type="hidden" name="loginType" value={register ? 'register' : 'login'}/>
+        <input
+          type="hidden"
+          name="loginType"
+          value={register ? "register" : "login"}
+        />
         <button onClick={handleClick}>
           {register
             ? "already have an account? click here"
--- app/utils/sessions.server.ts
+++ app/utils/sessions.server.ts
@@ -1,21 +1,37 @@
-import bcrypt from 'bcryptjs'
-import { PrismaClient } from '@prisma/client'
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export async function login({username, password}: {username: string, password: string}) {
-    const userWithPassword = await prisma.user.findUnique({
-        where: { username },
-        include: {password: true}
-    })
-    await prisma.$disconnect()
-    if (!userWithPassword || !userWithPassword.password?.hash) {
-        return null
-    }
-    const isValid = await bcrypt.compare(password, userWithPassword.password.hash)
-    if (!isValid) {
-        return null
-    }
-    const {password: _password, ...user} = userWithPassword
-    return user
+export async function login({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) {
+  let userWithPassword;
+  try {
+    userWithPassword = await prisma.user.findUnique({
+      where: { username },
+      include: { password: true },
+    });
+    await prisma.$disconnect();
+  } catch (err) {
+    console.log(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+  if (!userWithPassword || !userWithPassword.password?.hash) {
+    return null;
+  }
+  const isValid = await bcrypt.compare(
+    password,
+    userWithPassword.password.hash
+  );
+  if (!isValid) {
+    return null;
+  }
+  const { password: _password, ...user } = userWithPassword;
+  return user;
 }
