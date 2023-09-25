c01c817f1f2d0ffcb51ad23b535fde5810292062
Noah Clark <noahammon00@gmail.com>
2023-09-25 17:22:15 -0600

    added menu button component and modular css styling for the index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,30 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import MenuButton from "~/components/MenuButton";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Scrims Overwatch" },
-    { name: "description", content: "Setup scrim matches against people your rank in Overwatch 2!" },
+    {
+      name: "description",
+      content: "Setup scrim matches against people your rank in Overwatch 2!",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="flex justify-center" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="m-[1rem] text-[32pt]">Scrims Overwatch</h1>
-      <Link to="/AuthTest" className="absolute top-3 right-5">test</Link>
+    <div
+      className="flex justify-center items-center w-[100vw]"
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
+    >
+      <header className="flex justify-between items-center w-[100vw]">
+        <Link to="/AuthTest" className="mx-[1rem]">
+          test
+        </Link>
+        <h1 className="m-[1rem] text-[32pt]">Scrims Overwatch</h1>
+        <MenuButton/>
+      </header>
     </div>
   );
 }
