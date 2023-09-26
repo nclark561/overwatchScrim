49b6503409ba05f5a812e7ba68c39b8088cfeacf
Noah Clark <noahammon00@gmail.com>
2023-09-26 09:33:06 -0600

    added loader to logout route to redirect them to home page in case user tries to type in logout route in the url

--- app/routes/Logout.tsx
+++ app/routes/Logout.tsx
@@ -1,4 +1,6 @@
 import { logout } from "~/utils/sessions.server"
-import { ActionFunction } from "@remix-run/node"
+import { ActionFunction, LoaderFunction, redirect } from "@remix-run/node"
 
 export const action: ActionFunction = async ({ request }) => logout(request)
+
+export const loader: LoaderFunction = async () => redirect('/')
